2c1a869ebeb4ac0b89b0575e6da0ee10ecfcc007
imac 6 <imac6@imacs-iMac.local>
2023-05-05 17:51:19 +0300

    [wip] api for favourites

--- backend/backend/mock.py
+++ backend/backend/mock.py
@@ -2,7 +2,8 @@ from essential_generators import DocumentGenerator
 from datetime import date
 from random import randint
 from books.models import Book
-
+#
+import json
 
 # from randimage import get_random_image
 # from matplotlib import image
@@ -14,25 +15,34 @@ from books.models import Book
 # ../../../Desktop/randimage.png
 
 
+def fill_datefield(source_date):
+    if len(source_date) < 10:
+        return date(randint(1980, 2020), randint(1, 12), randint(1, 28))
+    return source_date
+
+
 def fill_database():
-    titles = []
-    authors = []
-    dg = DocumentGenerator()
+    # Opening JSON file
+    file = open('backend/books.json')
+
+    # returns JSON object as
+    # a dictionary
+    data = json.load(file)
+
     books = [
         Book(
-            title=dg.word(),  # TODO change it to normal title
-            description=dg.paragraph(),
+            title=data[i]['title'],  # 'title'
+            description=data[i]['description'],  # 'description'
             image='book_images/randimages/randimage_' + str(i) + '.png',
-            #   book_images/Qwer Asdf/Screenshot_2023-04-27_at_10.25.03.png
-            release_date=date(randint(1980, 2020), randint(1, 12), randint(1, 28)),
+            # release_date=data[i]['original_publication_date'],  # 'original_publication_date'
+            release_date=fill_datefield(data[i]['original_publication_date']),  # 'original_publication_date'
             price=randint(200, 800),
-            author=dg.name(),
-            # reviews_quantity = models.IntegerField(default=0)
+            author=data[i]['author_name'],  # 'author_name'
         )
-        for i in range(10)
+        for i in range(110)
     ]
     Book.objects.bulk_create(books)
-    # return 0
+    file.close()
 
 
 if __name__ == "__main__":
